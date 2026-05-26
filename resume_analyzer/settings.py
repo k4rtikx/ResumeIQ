4c07ad7968365c49c3af506f0e1c6584ab806e1f
kartik <kartiksingh1111116@gmail.com>
2026-05-27 00:20:57 +0530

    Configure DATABASE_URL support

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -108,11 +108,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
+            "NAME": os.getenv("DB_NAME", "resume_analyzer"),
+            "USER": os.getenv("DB_USER", "postgres"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT"),
+            "HOST": os.getenv("DB_HOST", "localhost"),
+            "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
 
